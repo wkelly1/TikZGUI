da9a672f6b17ee5cd10784e743923822fa4f2a7c
Will Kelly <william.kelly20@btinternet.com>
2021-02-12 22:39:46 +0000

    Added shadow

--- tikzgui-gui/src/main/java/org/tikzgui/gui/App.java
+++ tikzgui-gui/src/main/java/org/tikzgui/gui/App.java
@@ -18,6 +18,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         scene = new Scene(loadFXML("primary"), 640, 480);
+        scene.getStylesheets().add(getClass().getResource("css/main.css").toExternalForm());
         stage.setScene(scene);
         stage.show();
     }
@@ -28,6 +29,7 @@ public class App extends Application {
 
     private static Parent loadFXML(String fxml) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+      
         return fxmlLoader.load();
     }
 
