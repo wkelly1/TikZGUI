6553c7e5b969ac5b03a760563314fa8d7d39e1ea
alexmalt001 <=>
2021-02-12 21:58:17 +0000

    Fleshed out Rectangle representation

--- tikzgui-gui/src/main/java/org/tikzgui/core/Rectangle.java
+++ tikzgui-gui/src/main/java/org/tikzgui/core/Rectangle.java
@@ -2,9 +2,36 @@ package org.tikzgui.core;
 
 public class Rectangle extends GraphicsObject {
 	Stroke stroke;
+	Point topLeft;
+	Point topRight;
 
 	@Override
 	public PropertySet[] getProperties() {
 		return (new PropertySet[] {stroke});
+	}
+
+	public Stroke getStroke() {
+		return stroke;
+	}
+
+	public void setStroke(Stroke stroke) {
+		this.stroke = stroke;
+	}
+
+	public Point getTopLeft() {
+		return topLeft;
+	}
+
+	public void setTopLeft(Point topLeft) {
+		this.topLeft = topLeft;
+	}
+
+	
+	public Point getTopRight() {
+		return topRight;
+	}
+
+	public void setTopRight(Point topRight) {
+		this.topRight = topRight;
 	}
 }
