93e29e75bdfcd444d02fa11353444bc6b4c63d1a
alexmalt001 <=>
2021-02-12 23:02:06 +0000

    Implemented a Container class to house GraphicsObjects

---
+++ tikzgui-gui/src/main/java/org/tikzgui/core/Container.java
@@ -0,0 +1,7 @@
+package org.tikzgui.core;
+
+public abstract class Container {
+	
+	//returns the children contained within this container
+	public abstract GraphicsObject[] getChildren();
+}
---
+++ tikzgui-gui/src/main/java/org/tikzgui/core/Property.java
@@ -0,0 +1,5 @@
+package org.tikzgui.core;
+
+public abstract class Property {
+
+}
