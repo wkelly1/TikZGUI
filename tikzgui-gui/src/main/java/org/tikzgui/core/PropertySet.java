209dc795e5e664489d79fa0de47ca426af5090a7
alexmalt001 <=>
2021-02-12 22:37:40 +0000

    Implemented a skeleton Property class

--- tikzgui-gui/src/main/java/org/tikzgui/core/PropertySet.java
+++ tikzgui-gui/src/main/java/org/tikzgui/core/PropertySet.java
@@ -1,7 +1,5 @@
 package org.tikzgui.core;
 
-import java.util.HashMap;
-
 public abstract class PropertySet {
 	//returns all the properties contained within the set
 	public abstract Property[] getProperties();
