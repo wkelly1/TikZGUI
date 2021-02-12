2c1eb7c7fba4e8b472525d96bf6b5b48effe80fd
Matt <mi2u19@soton.ac.uk>
2021-02-12 23:12:22 +0000

    Fixed switch block

--- tikzgui-gui/src/main/java/org/tikzgui/texgen/Printer.java
+++ tikzgui-gui/src/main/java/org/tikzgui/texgen/Printer.java
@@ -2,9 +2,10 @@ public class Printer {
 
     public String print(gObject) {
         switch (gObject) {
-            case Rectangle:
+            case gObject.getClass() == Rectangle.class:
                 RectanglePrinter r = new RectanglePrinter(gObject); 
                 return r.print();
+                break;
         }
     }
 
