b262f36efc55ac51b2e26f594c08d869dcbd4949
Will Kelly <william.kelly20@btinternet.com>
2021-02-12 23:12:55 +0000

    We now have a canvas

--- tikzgui-gui/src/main/java/org/tikzgui/gui/PrimaryController.java
+++ tikzgui-gui/src/main/java/org/tikzgui/gui/PrimaryController.java
@@ -2,8 +2,13 @@ package org.tikzgui.gui;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.canvas.*;
 
 public class PrimaryController {
+    @FXML
+    private Canvas canvas;
+    
+    
 
     @FXML
     private void switchToSecondary() throws IOException {
