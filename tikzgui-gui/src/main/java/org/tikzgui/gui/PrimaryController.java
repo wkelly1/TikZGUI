8e903c1c6cfe50b74f3751359f2c04d520d3f096
Will Kelly <william.kelly20@btinternet.com>
2021-02-13 00:32:20 +0000

    Resizing added and canvas working

--- tikzgui-gui/src/main/java/org/tikzgui/gui/PrimaryController.java
+++ tikzgui-gui/src/main/java/org/tikzgui/gui/PrimaryController.java
@@ -2,16 +2,72 @@ package org.tikzgui.gui;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.canvas.*;
+import javafx.event.*;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 
-public class PrimaryController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class PrimaryController implements Initializable{
+    private double maxWidth = 0.0;
+    private double maxHeight = 0.0;
+
     @FXML
     private Canvas canvas;
-    
-    
+
+    @FXML
+    public GraphicsContext gc;
+
+    @FXML
+    public ScrollPane canvasParent;
+
+    public void initGraphics()
+    {
+        gc = canvas.getGraphicsContext2D();
+    }
+
+    @FXML
+    private void canvasClick(MouseEvent event) {
+        System.out.println("sdfjlkd");
+        canvas.getGraphicsContext2D().fillRect(75,75,100,100);
+    }
 
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
     }
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb)
+    {
+        canvas.addEventHandler(MouseEvent.MOUSE_CLICKED, (MouseEvent event) -> {
+            System.out.println(event);
+            gc.fillRect(event.getX(), event.getY(), 10.0, 10.0);
+        });
+
+        canvasParent.widthProperty().addListener((ov, oldValue, newValue) -> {
+
+            if (newValue.doubleValue() > this.maxWidth) {
+                System.out.println("resizing");
+                canvas.setWidth(newValue.doubleValue());
+                this.maxWidth = newValue.doubleValue();
+            }
+        });
+
+        canvasParent.heightProperty().addListener((ov, oldValue, newValue) -> {
+
+            if (newValue.doubleValue() > this.maxHeight) {
+                System.out.println("resizing");
+                canvas.setHeight(newValue.doubleValue());
+                this.maxHeight = newValue.doubleValue();
+            }
+        });
+
+
+        initGraphics();
+    }  
 }
