2e4047b7eb6c9e7dd42bc4941f02719ed60bdf93
Matt <mi2u19@soton.ac.uk>
2021-02-13 16:24:10 +0000

    Refactored Ellipse and rectangle printer

--- tikzgui-gui/src/main/java/org/tikzgui/texgen/EllipsePrinter.java
+++ tikzgui-gui/src/main/java/org/tikzgui/texgen/EllipsePrinter.java
@@ -5,17 +5,6 @@ import org.tikzgui.core.*;
 public class EllipsePrinter extends TeXElementPrinter <Ellipse> {
 	@Override
     public String print (Ellipse ellipse, Printer printer) {
-		String printedStroke = printer.print(ellipse.getStroke());
-		String printedEllipseProps = printer.print(ellipse.getEllipseProps());
-		String allProps;
-		if(printedStroke == "") {
-			allProps = printedEllipseProps;
-		} else {
-			allProps = printedStroke + ", " + printedEllipseProps;
-		}
-		
-		
-		
         String out = "\\draw ";
 		out += ellipse.getCentre().toString();
 		out += " ellipse ";
