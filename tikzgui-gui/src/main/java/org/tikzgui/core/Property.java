1e84836bdc6694c0592aa1a3041a108b7794c7b9
alexmalt001 <=>
2021-02-13 00:13:37 +0000

    made properties inheritable

--- tikzgui-gui/src/main/java/org/tikzgui/core/Property.java
+++ tikzgui-gui/src/main/java/org/tikzgui/core/Property.java
@@ -2,4 +2,6 @@ package org.tikzgui.core;
 
 public abstract class Property {
 
+	protected boolean inherit = false; // should this property inherit its value from parents/take the default value
+
 }
