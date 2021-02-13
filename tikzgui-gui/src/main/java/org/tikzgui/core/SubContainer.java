7e68338fd56ffe8619a6e608fd1bd92053b34bb6
alexmalt001 <=>
2021-02-13 00:06:03 +0000

    implemented inheritable properties for containers

---
+++ tikzgui-gui/src/main/java/org/tikzgui/core/SubContainer.java
@@ -0,0 +1,26 @@
+package org.tikzgui.core;
+
+import java.util.ArrayList;
+
+public abstract class SubContainer extends Container {
+	private Container parent;
+
+	public SubContainer (Container parent, GeneralProperties properties) {
+		super(properties);
+	}
+
+	public Container getParent() {
+		return parent;
+	}
+
+
+	//gets this objects properties, combined with those of its parent
+	@Override
+	public ArrayList<PropertySet> getProperties() {
+
+		ArrayList<PropertySet> allProperties = new ArrayList<>(parent.getProperties());
+		allProperties.addAll(super.getProperties());
+		return allProperties;
+	}
+
+}
