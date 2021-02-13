0bbc201279dfad6f973a4f0b8b19520c592925e5
alexmalt001 <alexmalt001@live.co.uk>
2021-02-13 15:40:04 +0000

    Added X and Y radii to EllipseProps

--- tikzgui-gui/src/main/java/org/tikzgui/core/EllipseProps.java
+++ tikzgui-gui/src/main/java/org/tikzgui/core/EllipseProps.java
@@ -1,7 +1,31 @@
 package org.tikzgui.core;
 
 public class EllipseProps extends PropertySet {
-
+	private XRadiusProperty xRadius;
+	private YRadiusProperty yRadius;
+	
+	public EllipseProps(XRadiusProperty xRadius, YRadiusProperty yRadius) {
+		this.xRadius = xRadius;
+		this.yRadius = yRadius;
+	}
+	
+	public XRadiusProperty getXRadius() {
+		return xRadius;
+	}
+	
+	public void setLineWidth(XRadiusProperty xRadius) {
+		this.xRadius = xRadius;
+	}
+	
+	public YRadiusProperty getYRadius() {
+		return yRadius;
+	}
+	
+	public void setLineWidth(YRadiusProperty yRadius) {
+		this.yRadius = yRadius;
+	}
+	
+	
 	@Override
 	public Property<?>[] getProperties() {
 		return (new Property[] {});
