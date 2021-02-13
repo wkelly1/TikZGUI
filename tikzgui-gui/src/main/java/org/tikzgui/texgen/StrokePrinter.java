f6941ec7bb064ff3f94f4dfe0b0e1264012049df
alexmalt001 <alexmalt001@live.co.uk>
2021-02-13 14:28:46 +0000

    Implemented StrokePrinter

--- tikzgui-gui/src/main/java/org/tikzgui/texgen/StrokePrinter.java
+++ tikzgui-gui/src/main/java/org/tikzgui/texgen/StrokePrinter.java
@@ -1,9 +1,15 @@
 package org.tikzgui.texgen;
 
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 import org.tikzgui.core.Stroke;
 
 public class StrokePrinter extends TeXElementPrinter<Stroke> {
     public String print(Stroke stroke, Printer printer) {
-        return "";
+        return Arrays.stream(stroke.getProperties()).parallel() //for each property of this Stroke
+        									.map(printer::print) //print each property
+        									.filter(s -> s!="") //filter out those that didn't return anything (i.e. their property will be inherited or use default)
+        									.collect(Collectors.joining(", ")); //append them all with a comma between each
     }
 }
