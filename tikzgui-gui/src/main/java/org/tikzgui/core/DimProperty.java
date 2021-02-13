2d086401ec40ea2ad367344e2f1dce98ec12b778
alexmalt001 <alexmalt001@live.co.uk>
2021-02-13 13:25:05 +0000

    Implemented typed properties

---
+++ tikzgui-gui/src/main/java/org/tikzgui/core/DimProperty.java
@@ -0,0 +1,4 @@
+package org.tikzgui.core;
+
+public class DimProperty extends Property<Double> {
+}
--- tikzgui-gui/src/main/java/org/tikzgui/core/Property.java
+++ tikzgui-gui/src/main/java/org/tikzgui/core/Property.java
@@ -1,7 +1,25 @@
 package org.tikzgui.core;
 
-public abstract class Property extends TeXElement {
+import java.util.Optional;
 
+public abstract class Property<T> extends TeXElement {
+	T value;
+	
 	protected boolean inherit = false; // should this property inherit its value from parents/take the default value
-
+	
+	public Property() {} //inherit
+	public Property(T value) {
+		this.value=value;
+	}
+	
+	public Optional<T> get() {
+		if (inherit)
+			return Optional.of(value);
+		else
+			return Optional.empty();
+	}
+	
+	public void set(T value) {
+		this.value = value;
+	}
 }
