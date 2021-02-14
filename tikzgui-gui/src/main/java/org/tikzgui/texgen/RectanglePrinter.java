e6193b8cfefae85e7d70f67523af0161524982e2
alexmalt001 <alexmalt001@live.co.uk>
2021-02-14 03:02:59 +0000

    fixed printPropertySetArray function

--- tikzgui-gui/src/main/java/org/tikzgui/texgen/RectanglePrinter.java
+++ tikzgui-gui/src/main/java/org/tikzgui/texgen/RectanglePrinter.java
@@ -6,8 +6,6 @@ import org.tikzgui.core.Rectangle;
 public class RectanglePrinter extends TeXElementPrinter<Rectangle> {
 	@Override
     public String print(Rectangle rectangle, Printer printer) {
-        String printedStroke = printer.print(rectangle.getStroke());
-        
         String out = "\\draw ";
         out += rectangle.getPointA().toString();
         out += " rectangle ";
--- tikzgui-gui/src/main/java/org/tikzgui/texgen/TeXElementPrinter.java
+++ tikzgui-gui/src/main/java/org/tikzgui/texgen/TeXElementPrinter.java
@@ -10,10 +10,9 @@ public abstract class TeXElementPrinter<T extends TeXElement> {
 	public abstract String print (T teXElement, Printer printer);
 	
 	protected String printPropertySetArray(PropertySet[] propSets, Printer printer) {
-		Arrays.stream(propSets).parallel() //for each property set in the array
+		return Arrays.stream(propSets).parallel() //for each property set in the array
 								.map(printer::print) //print each property
 								.filter(s -> s!="") //filter out those that didn't return anything (i.e. their property will be inherited or use default)
 								.collect(Collectors.joining(", ")); //append them all with a comma between each
-		return "";
 	}
 }
